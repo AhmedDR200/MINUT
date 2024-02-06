fb26f06760e3ee447bb2683ac94683ea52368468
AhmedDR200 <alshwwhy212@gmail.com>
2024-02-06 22:59:57 +0200

    Error Handling Setup

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+// Error handling
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
+
 // Load env variables
 dotenv.config();
 
@@ -20,8 +24,26 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 };
 
+// 404 Error Handling Middleware
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(new ApiError(`Can't find ${req.originalUrl} on this server`, 400));
+});
+
+// Global Error Handling Middleware
+app.use(globalError);
+
 // Server connection
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server (${process.env.NODE_ENV}) listening at http://localhost:${port}`)
+});
+
+// Events => Event Loop => Callback Queue => Event Loop => Event Handler
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err);
+  process.exit(1);
 });
